bc99974aa83e241ac11cd0be99a966ebbc020996
thumbert <humbert.tony@yahoo.com>
2017-12-10 19:43:55 -0500

    check for proper end of file

--- lib/src/iso/isone/lib_mis_reports.dart
+++ lib/src/iso/isone/lib_mis_reports.dart
@@ -2,12 +2,15 @@ library iso.isone.lib_mis_reports;
 
 import 'dart:io';
 import 'package:csv/csv.dart';
+import 'package:date/date.dart';
 
 /// Read/process MIS reports.
 /// Return each row of the [tab] as a List (all rows: C, H, D, T).
-List<List> readReport(File file, {int tab: 0}) {
+List<List> _readReport(File file, {int tab: 0}) {
   var converter = new CsvToListConverter();
   var lines = file.readAsLinesSync();
+  if (!lines.last.startsWith('"T"'))
+    throw 'Report is incomplete.  Aborting.';
   int nHeaders = 0;
   return lines
       .where((e) {
@@ -21,14 +24,28 @@ List<List> readReport(File file, {int tab: 0}) {
       .toList();
 }
 
+/// How to convert different columns.  CSV converter is pretty good with the
+/// numerical values.  I want to convert the dates, etc.
+Map conversions = {
+  /// ISO usually keeps the dates in mm/dd/yyyy format.
+  'Date': (String x) {
+    int year = int.parse(x.substring(6,10));
+    int month = int.parse(x.substring(0,2));
+    int day = int.parse(x.substring(3,5));
+    return new Date(year, month, day);
+  }
+};
+
 /// Read an MIS report and keep only the data rows, each row becoming a map,
 /// with keys taken from the header.
 /// If there are no data rows (empty report), return an empty List.
+///
 List<Map> readReportAsMap(File file, {int tab: 0}) {
-  List allData = readReport(file, tab: tab);
+  List allData = _readReport(file, tab: tab);
   List columnNames = allData.firstWhere((List e) => e[0] == 'H');
   return allData
       .where((List e) => e[0] == 'D')
       .map((List e) => new Map.fromIterables(columnNames, e))
       .toList();
 }
+
