ee7499f9436cf16ba24e80331b67aae30ee55faf
thumbert <humbert.tony@yahoo.com>
2018-03-04 14:23:07 -0500

    Update iso.dart

--- lib/src/iso/iso.dart
+++ lib/src/iso/iso.dart
@@ -16,12 +16,12 @@ class IsoNewEngland implements Iso {
   final String name = 'ISONE';
   static tz.Location location = tz.getLocation('US/Eastern');
 
-  static final bucket5x16    = new Bucket5x16(location);
-  static final bucket2x16H   = new Bucket2x16H(location);
-  static final bucket7x8     = new Bucket7x8(location);
-  static final bucket7x24    = new Bucket7x24(location);
-  static final bucketOffpeak = new BucketOffpeak(location);
-  static final bucketPeak    = new Bucket5x16(location);
+  static final Bucket bucket5x16    = new Bucket5x16(location);
+  static final Bucket bucket2x16H   = new Bucket2x16H(location);
+  static final Bucket bucket7x8     = new Bucket7x8(location);
+  static final Bucket bucket7x24    = new Bucket7x24(location);
+  static final Bucket bucketOffpeak = new BucketOffpeak(location);
+  static final Bucket bucketPeak    = new Bucket5x16(location);
 
   //static final Location hub = new NepoolLocation.fromPtid(4000)..name='Hub';
 
