63c3d56659ff6140ead35bc586a1191b664de6f0
thumbert <humbert.tony@yahoo.com>
2018-08-28 12:27:36 -0400

    Update auction.dart

--- lib/src/ftr/auction.dart
+++ lib/src/ftr/auction.dart
@@ -1,32 +1,51 @@
 library ftr.auction;
 
-import 'package:timezone/timezone.dart';
+import 'package:timezone/standalone.dart';
+import 'package:elec_server/src/utils/timezone_utils.dart';
+//import 'package:timezone/timezone.dart';
 import 'package:date/date.dart';
 import 'package:elec/elec.dart';
 
-class Auction {
-  String _name;
+enum FtrAuctionType {annual, monthly, bpp}
+
+/// A class representing an FTR Auction for ISONE.
+class FtrAuction {
+  Interval interval;
   Date start;
   /// number of months for this auction
   int noMonths;
   int round;
-  Iso iso;
-  int _noHours;
+  Location location = getLocation('US/Eastern');
+  String name;
+  RegExp _regExpAnnual = new RegExp(r'F(\d{2})-1Y-R(\d{1})');
+  FtrAuctionType auctionType;
 
+  /// Construct an Auction from an auction name.  Valid auction names are
+  /// 'F18-1Y-R1', 'F18-1Y-R2', etc. for annual auctions,
+  /// 'G18', etc. for monthly auctions.
+  FtrAuction.parse(this.name) {
+    if (name.length == 3) {
+      /// it's a monthly auction
+      var month = parseMonth(name, location: location) as Month;
+      interval = month.toInterval();
+      start = month.startDate;
+      noMonths = 1;
+      auctionType = FtrAuctionType.monthly;
 
-
-  Auction(this.iso, this.start, {this.noMonths: 1, this.round}) {
-//    if (round != null && round < 1)
-//      throw 'Invalid auction round: $round';
-    if (noMonths < 1)
-      throw 'Auction has $noMonths months?!';
-
+    } else if (name.contains('-')) {
+      /// it's an annual auction
+      var matches = _regExpAnnual.allMatches(name);
+      var match = matches.elementAt(0);
+      int year = 2000 + int.parse(match.group(1));
+      round = int.parse(match.group(2));
+      interval = new Interval(new TZDateTime(location, year),
+          new TZDateTime(location, year+1));
+      start = new Date(interval.start.year, interval.start.month, 1, location: location);
+      auctionType = FtrAuctionType.annual;
+    }
   }
 
-  /// Construct an Auction from a name.
-  Auction.fromName(String name) {
-    /// TODO: do it
-  }
+  String toString() => name;
 
   String _makeName() {
     StringBuffer res = new StringBuffer();
@@ -38,27 +57,4 @@ class Auction {
 
     return res.toString();
   }
-
-  String get name {
-    if (_name == null) _name = _makeName();
-    return _name;
-  }
-
-  /// Return the first hour of the Auction
-//  Hour firstHour() => new Hour.beginning(new TZDateTime(Iso.location, start.year, start.month));
-//
-//  /// Return the last hour of the Auction
-//  Hour lastHour() {
-//    var aux = new Month(start.year, start.month).add(noMonths);
-//    TZDateTime end = new TZDateTime(Iso.location, aux.year, aux.month);
-//    return new Hour.ending(end);
-//  }
-
-  /// Calculate the number of hours in this auction
-//  int get noHours {
-//    if (_noHours == null)
-//      _noHours = new TimeIterable(firstHour(), lastHour()).length;
-//    return _noHours;
-//  }
-
 }
