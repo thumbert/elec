8360acaf02418183f31929d1075beaf91835f16b
thumbert <humbert.tony@yahoo.com>
2018-09-27 10:07:29 -0400

    Update bucket_utils.dart

--- lib/src/time/bucket/bucket_utils.dart
+++ lib/src/time/bucket/bucket_utils.dart
@@ -7,7 +7,24 @@ import 'bucket.dart';
 /// each of these timeseries have intervals in a corresponding bucket.
 /// Only one traversal of [x] is made.  Works for hourly and sub-hourly
 /// timeseries.
-Map<Bucket,TimeSeries> splitByBucket(TimeSeries x, List<Bucket> buckets) {
-  //TODO
+Map<Bucket, List<IntervalTuple>> splitByBucket(
+    List<IntervalTuple> x, List<Bucket> buckets) {
+  Map<Bucket, List<IntervalTuple>> res = new Map.fromIterables(
+      buckets, new List.generate(buckets.length, (i) => []));
 
+  x.forEach((IntervalTuple e) {
+    buckets.forEach((Bucket bucket) {
+      if (bucket.containsHour(new Hour.beginning(e.interval.start)))
+        res[bucket].add(e);
+    });
+  });
+
+  /// remove empty buckets, if any
+  List emptyBuckets = [];
+  res.forEach((k, List v) {
+    if (v.isEmpty) emptyBuckets.add(k);
+  });
+  emptyBuckets.forEach((bucket) => res.remove(bucket));
+
+  return res;
 }
