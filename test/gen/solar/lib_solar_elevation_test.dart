d7fe06daf62ccdeddff3874db03cfd2d04a02379
thumberts <tony.humberts@gmail.com>
2020-04-26 21:29:36 -0400

    add a solar elevation function

---
+++ test/gen/solar/lib_solar_elevation_test.dart
@@ -0,0 +1,29 @@
+library test.load.solar.lib_solar_test;
+
+import 'package:elec/src/gen/solar/lib_solar_elevation.dart';
+import 'package:test/test.dart';
+import 'package:timezone/data/latest.dart';
+import 'package:timezone/standalone.dart';
+
+
+/// https://www.esrl.noaa.gov/gmd/grad/solcalc/
+void tests() {
+  group('Solar elevation tests: ', () {
+    var location = getLocation('US/Eastern');
+    test('elevation angle noon', () {
+      var dt = TZDateTime(location, 2010, 6, 21, 12);
+      expect(solarElevationAngle(40, -105, dt).toStringAsFixed(2), '48.48');
+    });
+    test('Boston', () {
+      var bos = coordinates['BOS'];
+      var dt = TZDateTime(location, 2020, 4, 25, 11);
+      var elevation = solarElevationAngle(bos.latitude, bos.longitude, dt);
+      expect(elevation.toStringAsFixed(2), '53.68');
+    });
+  });
+}
+
+void main() async {
+  await initializeTimeZones();
+  tests();
+}
