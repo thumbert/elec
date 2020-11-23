4931af2d30d9c689aad4c19a943f9104aaa63949
thumbert <humbert.tony@yahoo.com>
2020-11-23 17:17:15 -0500

    add a gasDay function

--- lib/src/physical/gas/timeseries_aggregation.dart
+++ lib/src/physical/gas/timeseries_aggregation.dart
@@ -4,6 +4,33 @@ import 'package:date/date.dart';
 import 'package:timeseries/timeseries.dart';
 import 'package:timezone/timezone.dart';
 
+final _location = getLocation('America/New_York');
+
+/// Calculate the NAESB gas day interval corresponding to an input TZDateTime.
+Interval gasDay(TZDateTime datetime) {
+  TZDateTime _start, _end;
+  // Calculation is done in the America/New_York time zone for convenience
+  var startNy = TZDateTime.fromMillisecondsSinceEpoch(_location,
+      datetime.millisecondsSinceEpoch); // bring it to the NY timezone
+  if (startNy.hour >= 10) {
+    _start =
+        TZDateTime(_location, startNy.year, startNy.month, startNy.day, 10);
+    _end =
+        TZDateTime(_location, startNy.year, startNy.month, startNy.day + 1, 10);
+  } else {
+    // it's the previous gas day
+    _start =
+        TZDateTime(_location, startNy.year, startNy.month, startNy.day - 1, 10);
+    _end = TZDateTime(_location, startNy.year, startNy.month, startNy.day, 10);
+  }
+  return Interval(
+    TZDateTime.fromMillisecondsSinceEpoch(
+        datetime.location, _start.millisecondsSinceEpoch),
+    TZDateTime.fromMillisecondsSinceEpoch(
+        datetime.location, _end.millisecondsSinceEpoch),
+  );
+}
+
 /// Convenience function to calculate a daily summary as specified by NAESB.
 /// The function [f] takes an Iterable of values and returns the summary
 /// statistic.  The TimeSeries [x] should not cross day boundaries.
@@ -12,31 +39,9 @@ import 'package:timezone/timezone.dart';
 /// Implementation is more efficient than the simple groupByIndex + map.
 TimeSeries<T> toGasDay<K, T>(
     Iterable<IntervalTuple<K>> xs, T Function(List<K>) f) {
-  var location = getLocation('America/New_York');
   var grp = <Interval, List<K>>{};
   for (var x in xs) {
-    var start = x.interval.start;
-    TZDateTime _start, _end;
-    // Calculation is done in the America/New_York time zone
-    var startNy = TZDateTime.fromMillisecondsSinceEpoch(location,
-        x.interval.start.millisecondsSinceEpoch); // bring it to the NY timezone
-    if (startNy.hour >= 10) {
-      _start =
-          TZDateTime(location, startNy.year, startNy.month, startNy.day, 10);
-      _end = TZDateTime(
-          location, startNy.year, startNy.month, startNy.day + 1, 10);
-    } else {
-      _start = TZDateTime(
-          location, startNy.year, startNy.month, startNy.day - 1, 10);
-      _end = TZDateTime(location, startNy.year, startNy.month, startNy.day, 10);
-    }
-    var interval = Interval(
-      TZDateTime.fromMillisecondsSinceEpoch(
-          start.location, _start.millisecondsSinceEpoch),
-      TZDateTime.fromMillisecondsSinceEpoch(
-          start.location, _end.millisecondsSinceEpoch),
-    );
-
+    var interval = gasDay(x.interval.start);
     grp.putIfAbsent(interval, () => <K>[]).add(x.value);
   }
   return TimeSeries.from(grp.keys, grp.values.map((ys) => f(ys)));
