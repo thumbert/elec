4e465ffda645ee94eae059ee8843bbd3972281d3
thumbert <humbert.tony@yahoo.com>
2021-12-12 20:39:40 -0500

    improve exports

--- lib/calculators/weather.dart
+++ lib/calculators/weather.dart
@@ -5,3 +5,6 @@ export '../src/risk_system/pricing/calculators/weather/daily_temperature_option.
 export '../src/risk_system/pricing/calculators/weather/index_option.dart';
 export '../src/risk_system/pricing/calculators/weather/index_swap.dart';
 export '../src/risk_system/pricing/calculators/weather/weather_instrument.dart';
+
+export '../src/weather/leap_year_policy.dart';
+export '../src/weather/lib_weather_utils.dart';
