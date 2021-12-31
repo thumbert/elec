d66a4ccd9765ac56770daf1d922ab8f6d2bd638c
thumbert <humbert.tony@yahoo.com>
2021-12-30 23:25:00 -0500

    add gas products

--- lib/risk_system.dart
+++ lib/risk_system.dart
@@ -20,6 +20,7 @@ part 'src/risk_system/call_put.dart';
 part 'src/risk_system/commodity.dart';
 part 'src/risk_system/energy_hubs.dart';
 part 'src/risk_system/energy_unit.dart';
+part 'src/risk_system/gas_product.dart';
 part 'src/risk_system/lmp_component.dart';
 part 'src/risk_system/market.dart';
 part 'src/risk_system/quantity.dart';
