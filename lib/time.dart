bcfb1386588b149f8af9851621b72729d38bc591
thumbert <tony.humberts@gmail.com>
2024-07-06 06:45:06 -0400

    2024-07-06

--- lib/time.dart
+++ lib/time.dart
@@ -1,12 +1,10 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 library time;
 
 export 'src/time/bucket/bucket.dart';
 export 'src/time/bucket/bucket_utils.dart';
 export 'src/time/calendar/calendar.dart';
 export 'src/time/calendar/holiday.dart';
+export 'src/time/day_filter.dart';
 export 'src/time/hour_filter.dart';
 export 'src/time/hourly_schedule.dart';
 export 'src/time/last_trading_day.dart';
