9eadd0b7dd4756c82c6f1dd92e442ca1e6605fd6
thumbert <tony.humberts@gmail.com>
2024-09-18 07:19:40 -0400

    minor

--- lib/src/price/lib_lmp.dart
+++ lib/src/price/lib_lmp.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 
 import 'package:collection/collection.dart';
 import 'package:date/date.dart';
-import 'package:decimal/decimal.dart';
 import 'package:duckdb_dart/duckdb_dart.dart';
 import 'package:elec/risk_system.dart';
 import 'package:elec/src/iso/iso.dart';
@@ -20,52 +19,37 @@ Map<Ptid, TimeSeries<num>> getHourlyLmpIsone(
     required Term term}) {
   assert(term.location == IsoNewEngland.location);
   final con = Connection(
-      '${Platform.environment['HOME']}/Downloads/Archive/IsoExpress/da_lmp.duckdb',
+      '${Platform.environment['HOME']}/Downloads/Archive/IsoExpress/${market.name.toLowerCase()}.duckdb',
       Config(accessMode: AccessMode.readOnly));
   var cname = switch (component.name) {
     'lmp' => 'lmp',
     'congestion' => 'mcc',
     'loss' => 'mlc',
-    _ => throw StateError('Invalid component $component');
+    _ => throw StateError('Invalid component $component')
   };
   final query = """
 SELECT ptid, date, hour, extraDstHour, $cname
-FROM da_lmp
+FROM ${market.name.toLowerCase()}_lmp
 WHERE date >= '${term.startDate}'
 AND date <= '${term.endDate}'
 AND ptid in (${ptids.join(", ")})
 ORDER BY ptid, date, hour, extraDstHour;
 """;
-  // print(query);
-  var data = con.fetch(query);
+  var data = con.fetchRows(query, (List row) {
+    final date = Date.fromJulianDay(row[1]);
+    var interval = Hour.beginning(TZDateTime(
+        IsoNewEngland.location, date.year, date.month, date.day, row[2]));
+    if (row[3]) {
+      interval = interval.next;
+    }
+    return (row[0] as int, IntervalTuple<num>(interval, row[4].toDouble()));
+  });
   con.close();
-  // print(data);
-
-  final ptid = data['ptid']!.cast<int>();
-  final dates = data['date']!;
-  final hour = data['hour']!;
-  final extraDstHour = data['extraDstHour']!;
-  final price = data['lmp']!;
 
   var res = <int, TimeSeries<num>>{};
-  var groups = groupBy<(int, int), int>(ptid.indexed, (e) => e.$2);
+  var groups = groupBy(data, (e) => e.$1);
   for (var ptid in groups.keys) {
-    var ids = groups[ptid]!;
-    var one = TimeSeries<num>();
-    for (var id in ids) {
-      var p = price[id.$1];
-      if (p != null) {
-        final date = Date.fromJulianDay(dates[id.$1]! as int);
-        var interval = Hour.beginning(TZDateTime(IsoNewEngland.location,
-            date.year, date.month, date.day, hour[id.$1]! as int));
-        if (extraDstHour[id.$1]! as bool) {
-          interval = interval.next;
-        }
-        one.add(IntervalTuple<num>(interval, (p as Decimal).toDouble()));
-      }
-    }
-    res[ptid] = one;
+    res[ptid] = TimeSeries.fromIterable(groups[ptid]!.map((e) => e.$2));
   }
-
   return res;
 }
