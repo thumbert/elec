192c95a56dd4820ac14e03fa29ace96d3ef6ea36
thumbert <tony.humberts@gmail.com>
2026-07-06 08:03:13 -0400

    2026-07-06

--- lib/nyiso.dart
+++ lib/nyiso.dart
@@ -1,1 +1,3 @@
 export 'src/iso/nyiso/capacity/location.dart';
+export 'src/iso/nyiso/capacity/season.dart';
+
---
+++ lib/src/iso/nyiso/capacity/season.dart
@@ -0,0 +1,38 @@
+import 'package:date/date.dart';
+import 'package:elec/src/iso/iso.dart';
+
+enum PeriodType { summer, winter }
+
+class CapacitySeason {
+  /// Construct a capacity season from a name like 'Summer 2026' or 'Winter 2026-2027'.
+  /// The name should be in the format 'Summer YYYY' or 'Winter YYYY-YYYY'.
+  CapacitySeason(this.name) {
+    final aux = name.split(' ');
+    periodType = switch (aux[0]) {
+      'Summer' => PeriodType.summer,
+      'Winter' => PeriodType.winter,
+      _ => throw ArgumentError('Unknown PeriodType: ${aux[0]}')
+    };
+    final yearStart = int.parse(aux[1].substring(0, 4));
+
+    final startDate = switch (periodType) {
+      PeriodType.summer => Date(yearStart, 5, 1, location: NewYorkIso.location),
+      PeriodType.winter => Date(yearStart, 11, 1, location: NewYorkIso.location)
+    };
+    final endDate = switch (periodType) {
+      PeriodType.summer =>
+        Date(yearStart, 10, 31, location: NewYorkIso.location),
+      PeriodType.winter =>
+        Date(yearStart + 1, 4, 30, location: NewYorkIso.location)
+    };
+    term = Term(startDate, endDate);
+  }
+
+  /// 'Summer 2026' or 'Winter 2026-2027'
+  late final String name;
+
+  /// In America/New_York timezone
+  late final Term term;
+
+  late final PeriodType periodType;
+}
